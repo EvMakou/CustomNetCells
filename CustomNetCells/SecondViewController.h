a2fb3836b73d70baf026dcceeaa1a4faf72d3d9e
supermacho <face_buk@mail.ru>
2017-10-06 03:12:34 +0300

    Fix project

---
+++ CustomNetCells/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  CustomNetCells
+//
+//  Created by supermacho on 05.10.17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
